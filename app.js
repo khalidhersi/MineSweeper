214dafac87925d099290cfa97ee916b42cc9d74d
khalidhersi <cipherpolfoundation@gmail.com>
2021-11-13 21:25:18 +0000

    javascript all variables declared

---
+++ app.js
@@ -0,0 +1,21 @@
+const clock = document.querySelector(".UI__clock");
+const counter = document.querySelector(".UI__counter");
+const buttons = document.querySelectorAll(".grif__items");
+
+const timer = 0;
+
+const updateTimer = () => {
+    timer += 1;
+};
+
+setInterval(updateTimer, 1000)
+
+clock.innerHTML = 
+
+counter.addEventListener("click" , event => {
+
+});
+
+buttons.addEventListener("click" , event => {
+
+});
