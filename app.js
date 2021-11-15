d521f9a48201119877eadfce42f82c7d14fbbc07
khalidhersi <cipherpolfoundation@gmail.com>
2021-11-15 17:39:10 +0000

    result after bomb is found

--- app.js
+++ app.js
@@ -4,31 +4,33 @@ const buttons = document.querySelectorAll(".grid__items");
 const gameBoard = document.querySelector(".grid-container")
 
 /* create grid items */
-    for (let i = 0; i < 30; i++){
-       const items = document.createElement("button")
-       gameBoard.appendChild(items)
+for (let i = 0; i < 30; i++){
+    const items = document.createElement("button")
+    gameBoard.appendChild(items)
+    if (i == this.bombLocation) {
+        items.setAttribute('value', bomb)
+        items.addEventListener("click", () => alert("bomb") )
     }
-
-    
-/* bomb location array */
-bombArr = [] 
-for (i = 0; i <= 5; i++){
-    let locationGenerator = Math.floor(Math.random() * 30)
-    bombArr.push(locationGenerator)
-    bombObj = {
-        value: bombArr[i]
-    }
-    const bombLocation = bombObj.value
-    console.log(bombLocation)
+     items.setAttribute('value', i)
 }
-
-
+bombArr = [] 
+    for (i = 0; i <= 5; i++){
+        let locationGenerator = Math.floor(Math.random() * 30)
+        bombArr.push(locationGenerator)
+        bombObj = {
+            value: bombArr[i]
+        }
+        const bombLocation = bombObj.value
+        console.log(bombLocation)
+    }
 itemObj = {
 
 }
+    
+/* bomb location array */
+
 
 /*  grid items location array */
-
 
 
 
