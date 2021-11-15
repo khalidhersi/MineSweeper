5b2e8a69f7c5d9ea63c84049f5b1dfad470b5295
khalidhersi <cipherpolfoundation@gmail.com>
2021-11-15 16:52:57 +0000

    making bombs

--- app.js
+++ app.js
@@ -1,16 +1,62 @@
 const clock = document.querySelector(".UI__clock");
 const counter = document.querySelector(".UI__counter");
-const buttons = document.querySelectorAll(".grif__items");
+const buttons = document.querySelectorAll(".grid__items");
+const gameBoard = document.querySelector(".grid-container")
+
+/* create grid items */
+    for (let i = 0; i < 30; i++){
+       const items = document.createElement("button")
+       gameBoard.appendChild(items)
+    }
+
+    
+/* bomb location array */
+bombArr = [] 
+for (i = 0; i <= 5; i++){
+    let locationGenerator = Math.floor(Math.random() * 30)
+    bombArr.push(locationGenerator)
+    bombObj = {
+        value: bombArr[i]
+    }
+    const bombLocation = bombObj.value
+    console.log(bombLocation)
+}
+
+
+itemObj = {
+
+}
+
+/*  grid items location array */
+
+
+
+
+
+/*
+items.setAttribute('value', i)
+       itemArr = Array(items)
+       itemArr.forEach(item => {
+       });
+
+
+
+gridItem = {
+    []
+}
+
 
 const timer = 0;
-
 const updateTimer = () => {
     timer += 1;
+     clock.innerHTML = timer
+    setInterval(timer, 100)
 };
 
-setInterval(updateTimer, 1000)
+
 
 clock.innerHTML = 
+
 
 counter.addEventListener("click" , event => {
 
@@ -19,3 +65,4 @@ counter.addEventListener("click" , event => {
 buttons.addEventListener("click" , event => {
 
 });
+*/
